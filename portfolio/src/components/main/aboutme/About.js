dbc0cb095688b6416b6725da516b3cb13e1a0cd4
BrandyBecker <brandybeckerfsw@gmail.com>
2020-09-29 21:18:00 -0400

    about section & some styling

---
+++ portfolio/src/components/main/aboutme/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="About">
+      <h1 class="aboutmetitle">About the Developer</h1>
+      <p class="paragraph"><span class="title">Hey, I'm Brandy! </span><br></br> <br></br>I'm a really energetic & bubbly person who can make anyone feel right at home. I've had a passion for software development & technology for as far back as I can remember. <br></br> As a child, I always liked tinkering with technology 💾! <br></br>In High School I was on a team of six for Vex Robotics ( 6135C 🤖 ) & placed 20th in the world in 2016. From there I dove deeper & deeper into the programming web - first picking up HTML, CSS, Lua & C. I attended an online coding bootcamp ( thanks Lambda! ) & spent over 2000hrs+ of time coding & where I further developed my skills in HTML & CSS, & picked up several other languages & framework, and can now say I'm more than comfortable with developing any Full Stack Web Application thrown my way! Check out some of my work below, & if you have any questions, please reach out & contact me! Thanks!</p>
+    </div>
+  );
+}
+
+export default About;
