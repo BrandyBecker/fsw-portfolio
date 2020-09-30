2689c52b6286bf19c124e0b469cff8a97de7248e
BrandyBecker <brandybeckerfsw@gmail.com>
2020-09-30 02:37:45 -0400

    small updates to skills styling

--- portfolio/src/components/main/skills/Skills.js
+++ portfolio/src/components/main/skills/Skills.js
@@ -7,19 +7,19 @@ function Skills() {
     <h1 class="skillstitle">Skills</h1>
       <div class="skillssections">
         <div class="frontend skill">
-            <h1><i class="fas fa-laptop-code"></i> Front End</h1>
+            <h1><i class="fas fa-laptop-code"></i> <span class="skillname">Front End</span></h1>
             <div class="skills">
                 HTML5 | CSS3 | JavaScript | React | Redux | TypeScript | LESS | SASS | Styled Components | Semantic UI | Material UI | Bootstrap | Reactstrap | Axios | Fetch | Netlify | Zeit | Git | Jest | React-Testing-Library
             </div>
         </div>
         <div class="backend skill">
-            <h1><i class="fas fa-server"></i> Back End</h1>
+            <h1><i class="fas fa-server"></i> <span class="skillname">Back End</span></h1>
             <div class="skills">
                 Node | Python | REST | SQL | GraphQL | Express | KNEX | Heroku | Jest | SuperTest
             </div>
         </div>
         <div class="mobile skill">
-            <h1><i class="fas fa-mobile-alt"></i> Mobile</h1>
+            <h1><i class="fas fa-mobile-alt"></i> <span class="skillname">Mobile</span></h1>
             <div class="skills">
                 React Native | TypeScript | React | Redux | React-Testing-Library | ExpoCLI
             </div>
